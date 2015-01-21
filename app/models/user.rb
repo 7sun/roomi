278fefe9deba9ced67c08ee7a09877f87ed52297
7sun <pakrouse@gmail.com>
2015-01-20 21:33:39 -0800

    adds user model and signup view

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User
+  include Mongoid::Document
+  include ActiveModel::SecurePassword
+
+  has_secure_password
+
+  field :email, type: String
+  field :password_digest, type: String
+
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :password, confirmation: true, length: { :within => 6..30 }
+
+end
